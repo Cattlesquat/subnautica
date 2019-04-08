b03dc3161881435a9f9873b9c9ca5e1e44b93933
Seraphim Dibble <dibblebill@gmail.com>
2019-04-08 10:24:14 -0600

    Cleaned up directories and fixed all the extra DLL's.
    Also added code that should allow decompression while in submarines and habitats and other places. While this means taking damage/getting punished for long excursions, it should also not be lethal amounts or get a player killed when returning to the surface.
    
    Some warnings will need to be adjusted.

--- NitrogenMod/Patchers/NitroLevPatcher.cs
+++ NitrogenMod/Patchers/NitroLevPatcher.cs
@@ -22,7 +22,7 @@
     [HarmonyPatch("Update")]
     internal class NitroDamagePatcher
     {
-        private static int frametimer = 0;
+        //private static int frametimer = 0;
 
         [HarmonyPrefix]
         public static bool Prefix (ref NitrogenLevel __instance)
@@ -30,29 +30,29 @@
             if (__instance.nitrogenEnabled)
             {
                 float depthOf = Ocean.main.GetDepthOf(Player.main.gameObject);
-                if (__instance.GetLevelsDangerous() && UnityEngine.Random.value < 0.025f)
+                if ((__instance.GetLevelsDangerous() || Player.main.motorMode != Player.MotorMode.Dive) && UnityEngine.Random.value < 0.025f)
                 {
                     global::Utils.Assert(depthOf < __instance.safeNitrogenDepth, "see log", null);
                     LiveMixin component = Player.main.gameObject.GetComponent<LiveMixin>();
                     ErrorMessage.AddMessage("Nitrogen level unsafe. Ascend slower.");
                     component.TakeDamage(1f + 1.5f * (__instance.safeNitrogenDepth - depthOf) / __instance.kDepthInterval, default(Vector3), DamageType.Normal, null);
                 }
-                if (depthOf < __instance.safeNitrogenDepth)
+                float num = 1f;
+                if (depthOf < __instance.safeNitrogenDepth && Player.main.motorMode == Player.MotorMode.Dive)
                 {
-                    float num = 1f;
-                    if (Player.main.motorMode == Player.MotorMode.Dive)
-                    {
-                        num = Mathf.Clamp(2f - __instance.GetComponent<Rigidbody>().velocity.magnitude, 0f, 2f) * 1f;
-                    }
-                    else
-                    {
-                        num = 2f;
-                    }
+                    num = Mathf.Clamp(2f - __instance.GetComponent<Rigidbody>().velocity.magnitude, 0f, 2f) * 1f;
                     __instance.safeNitrogenDepth = UWE.Utils.Slerp(__instance.safeNitrogenDepth, depthOf, __instance.kDissipateScalar * num * Time.deltaTime);
-                    //ErrorMessage.AddMessage("safeNitrogenDepth: " + __instance.safeNitrogenDepth.ToString());
+                    ErrorMessage.AddMessage("safeNitrogenDepth: " + __instance.safeNitrogenDepth.ToString());
+                }
+                else if (Player.main.motorMode != Player.MotorMode.Dive)
+                {
+                    num = Mathf.Clamp(2f - __instance.GetComponent<Rigidbody>().velocity.magnitude, 0f, 2f) * 1f;
+                    __instance.safeNitrogenDepth = UWE.Utils.Slerp(__instance.safeNitrogenDepth, depthOf, __instance.kDissipateScalar * num * Time.deltaTime);
+                    ErrorMessage.AddMessage("safeNitrogenDepth: " + __instance.safeNitrogenDepth.ToString());
                 }
                 /*
                  * Silly me. Bends aren't a think in submarines. If they were, this code would definitely let you undergo compression as you explore.
+                 * This code can be repurposed, though.
                  *Scene menuCheck = SceneManager.GetActiveScene();
                  *string isInMenu = menuCheck.name;
                  *if (depthOf > __instance.safeNitrogenDepth && Player.main.motorMode != Player.MotorMode.Dive && !isInMenu.ToLower().Contains("menu"))
