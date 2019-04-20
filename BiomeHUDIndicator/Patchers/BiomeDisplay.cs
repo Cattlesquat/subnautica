b67ef9e945ffcb789e4ad426d7c4ce5e98cea4ab
Seraphim Dibble <dibblebill@gmail.com>
2019-04-20 17:55:24 -0600

    It updates the biome precisely once.

--- BiomeHUDIndicator/Patchers/BiomeDisplay.cs
+++ BiomeHUDIndicator/Patchers/BiomeDisplay.cs
@@ -12,26 +12,34 @@
     class BiomeDisplay : MonoBehaviour
     {
         public GameObject BiomeHUDObject { private get; set; }
+        public GameObject _BiomeHUDObject { private get; set; }
         private static float t = 0f;
         private static BiomeDisplay main;
 
         
         private void Awake()
         {
-            BiomeDisplay.main = this;
-            BiomeHUDObject = Instantiate(Main.BiomeHUD);
-            BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled = false;
+            BiomeHUDObject = Main.BiomeHUD;
+            //Instantiate(BiomeHUDObject);
+            //BiomeHUDObject.SetActive(false);
+            //BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled = false;
             t = Time.time;
             SeraLogger.Message(Main.modName, "BiomeDisplay.Awake() has run. BiomeDisplay is awake and running!");
+            SeraLogger.Message(Main.modName, "BiomeHUDObject.activeSelf: " + BiomeHUDObject.activeSelf.ToString());
+            SeraLogger.Message(Main.modName, "BiomeHUDObject.activeInHierarchy: " + BiomeHUDObject.activeInHierarchy.ToString());
+            BiomeDisplay.main = this;
         }
 
         private void Update()
         {
-            if (Time.time >= t + 5f && BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled == true)
+            float f = Time.time;
+            if (f >= t + 5f && _BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled == true)
             {
-                SeraLogger.Message(Main.modName, "Entered Time check in Update");
-                BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled = false;
-                SeraLogger.Message(Main.modName, "BiomeHUDObject.BiomeHUDChip.Text.enabled: " + BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled.ToString());
+                SeraLogger.Message(Main.modName, "Destroying the instantiated object");
+                Destroy(_BiomeHUDObject);
+                //BiomeHUDObject.SetActive(false);
+                //BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled = false;
+                t = Time.time;
             }
         }
 
@@ -39,10 +47,14 @@
         {
             SeraLogger.Message(Main.modName, "message: " + message);
             BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().text = message;
-            BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled = true;
+            _BiomeHUDObject = BiomeHUDObject;
+            Instantiate(_BiomeHUDObject);
+            //BiomeHUDObject.SetActive(true);
+            //BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled = true;
+            SeraLogger.Message(Main.modName, "ChangeText() BiomeHUDObject.BiomeHUDChip.Text.text: " + BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().text);
+            SeraLogger.Message(Main.modName, "ChangeText() BiomeHUDObject.BiomeHUDChip.Text.enabled: " + BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled.ToString());
             t = Time.time;
-            SeraLogger.Message(Main.modName, "BiomeHUDObject.BiomeHUDChip.Text.text: " + BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().text);
-            SeraLogger.Message(Main.modName, "BiomeHUDObject.BiomeHUDChip.Text.enabled: " + BiomeHUDObject.transform.Find("BiomeHUDChip").gameObject.GetComponent<Text>().enabled.ToString());
+            SeraLogger.Message(Main.modName, "t: " + t.ToString());
         }
 
         public static void DisplayBiome(string message)
