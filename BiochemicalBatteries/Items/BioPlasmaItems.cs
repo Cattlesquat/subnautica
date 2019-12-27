780916dcd8803f200ae1224438a503ad82c8ed39
Seraphim Dibble <dibblebill@gmail.com>
2019-12-27 13:38:06 -0700

    BiochemicalBatteries Setup
    Set up what will become the biochemical batteries mod.

---
+++ BiochemicalBatteries/Items/BioPlasmaItems.cs
@@ -0,0 +1,51 @@
+﻿namespace BiochemicalBatteries.Items
+{
+    using SMLHelper.V2.Assets;
+    using SMLHelper.V2.Handlers;
+    using UnityEngine;
+
+    internal abstract class BioPlasmaItems : Spawnable
+    {
+        public static TechType BioPlasmaID { get; protected set; }
+
+        internal static void PatchBioPlasmaItems()
+        {
+            var bioPlasma = new BioPlasmaCore();
+
+            bioPlasma.Patch();
+
+            CraftDataHandler.SetHarvestOutput(TechType.Warper, BioPlasmaID);
+            CraftDataHandler.SetHarvestType(TechType.Warper, HarvestType.DamageDead);
+        }
+
+        protected abstract TechType BaseType { get; }
+
+        protected BioPlasmaItems(string classID, string friendlyName, string description) : base(classID, friendlyName, description)
+        {
+
+        }
+
+        public override GameObject GetGameObject()
+        {
+            GameObject prefab = CraftData.GetPrefabForTechType(this.BaseType);
+            var obj = GameObject.Instantiate(prefab);
+
+            return obj;
+        }
+    }
+
+    class BioPlasmaCore : BioPlasmaItems
+    {
+        public BioPlasmaCore()
+            : base(classID: "bioplasmacore", friendlyName: "Bioplasma Core", description: "The power core from a dead Warper.")
+        {
+            OnFinishedPatching += SetStaticTechType;
+        }
+
+        protected override TechType BaseType { get; } = TechType.PrecursorIonCrystal;
+
+        public override string AssetsFolder { get; } = @"EnzymeChargedBattery/Assets";
+
+        private void SetStaticTechType() => BioPlasmaID = this.TechType;
+    }
+}
---
+++ BiochemicalBatteries/Main.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// This mod serves as an example on how to use PrimeSonic's CustomBatteries mod in combination with other patches to create a more complex project.
+/// 
+/// </summary>
+
+namespace BiochemicalBatteries
+{
+    using System;
+    using System.Reflection;
+    using Harmony;
+    using Items;
+    using Common;
+
+    public static class Main
+    {
+        public const string modName = "[BiochemicalBattery]";
+
+        public static void Patch()
+        {
+            SeraLogger.PatchStart(modName, "1.0.0");
+            try
+            {
+                var harmony = HarmonyInstance.Create("seraphimrisen.biochemicalbattery.mod");
+
+                BioPlasmaItems.PatchBioPlasmaItems();
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+                SeraLogger.PatchComplete(modName);
+            }
+            catch (Exception ex)
+            {
+                SeraLogger.PatchFailed(modName, ex);
+            }
+        }
+    }
+}
